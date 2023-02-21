049b1d3cf395b548277f8bba645fe4c99f783707
alexlysak2688@gmail.com <alexlysak2688@gmail.com>
2023-02-21 22:07:20 +0200

    add font-family Raleway and add marginBottom

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -2,17 +2,25 @@ import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
 import CssBaseline from '@mui/material/CssBaseline'
-import { StyledEngineProvider } from '@mui/material/styles'
-
+import { createTheme, StyledEngineProvider} from '@mui/material/styles'
+import { ThemeProvider } from '@mui/system'
 type Props = {}
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Raleway, sans-serif',
+  },
+})
 
 const App = (props: Props) => {
     return (
         <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={theme}>
             <CssBaseline />
             <Header />
             <Main />
             <Footer />
+            </ThemeProvider>
         </StyledEngineProvider>
     )
 }
