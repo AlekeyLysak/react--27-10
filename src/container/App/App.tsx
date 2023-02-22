9205272cb413b531d302393c0312a71fd3d6635f
alexlysak2688@gmail.com <alexlysak2688@gmail.com>
2023-02-22 16:08:44 +0200

    add cartData with totalCount and total Price

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -2,24 +2,35 @@ import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
 import CssBaseline from '@mui/material/CssBaseline'
-import { createTheme, StyledEngineProvider} from '@mui/material/styles'
+import { createTheme, StyledEngineProvider } from '@mui/material/styles'
 import { ThemeProvider } from '@mui/system'
+import { useState } from 'react'
+
 type Props = {}
 
 const theme = createTheme({
-  typography: {
-    fontFamily: 'Raleway, sans-serif',
-  },
+    typography: {
+        fontFamily: 'Raleway, sans-serif',
+    },
 })
 
+type CartDataProps = {
+    totalCount: number
+    totalPrice: number
+}
+
 const App = (props: Props) => {
+    const [cartData, setCartData] = useState<CartDataProps>({
+        totalCount: 10,
+        totalPrice: 100,
+    })
     return (
         <StyledEngineProvider injectFirst>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Header />
-            <Main />
-            <Footer />
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Header cartData={cartData} />
+                <Main />
+                <Footer />
             </ThemeProvider>
         </StyledEngineProvider>
     )
